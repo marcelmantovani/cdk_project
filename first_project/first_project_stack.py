7885cd7dcfeb417657f4061884742ae6d26cf17e
Marcel Mantovnai Filho <37183770+marcelmantovani@users.noreply.github.com>
2021-06-12 21:59:46 -0400

    setting up gitignore and initial code

--- first_project/first_project_stack.py
+++ first_project/first_project_stack.py
@@ -1,4 +1,6 @@
-from aws_cdk import core as cdk
+from aws_cdk import (
+    aws_s3 as _s3,
+    core as cdk)
 
 # For consistency with other languages, `cdk` is the preferred import name for
 # the CDK's core module.  The following line also imports it as `core` for use
@@ -13,3 +15,9 @@ class FirstProjectStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "firstBucketId",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
