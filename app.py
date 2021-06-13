15eccdb6ee5ee3d053e317da53eb48104a1d8c4c
Marcel Mantovnai Filho <37183770+marcelmantovani@users.noreply.github.com>
2021-06-13 00:19:57 -0400

    tags and stack-level tags

--- app.py
+++ app.py
@@ -19,26 +19,29 @@ app = core.App()
 
 customVpc = CustomVpcStack(app, "custom-vpc-stack")
 
+cdk.Tags.of(app).add("support-email-contact",
+                     app.node.try_get_context('envs')['prod']['support-email-contact'])
+
 
 # env_US = core.Environment(region=app.node.try_get_context('envs')['prod']['region'])
 # print(app.node.try_get_context('envs')['prod']['region'])
 # FirstProjectStack(app, "DevProjectStack", env=env_US
-                  # If you don't specify 'env', this stack will be environment-agnostic.
-                  # Account/Region-dependent features and context lookups will not work,
-                  # but a single synthesized template can be deployed anywhere.
+# If you don't specify 'env', this stack will be environment-agnostic.
+# Account/Region-dependent features and context lookups will not work,
+# but a single synthesized template can be deployed anywhere.
 
-                  # Uncomment the next line to specialize this stack for the AWS Account
-                  # and Region that are implied by the current CLI configuration.
+# Uncomment the next line to specialize this stack for the AWS Account
+# and Region that are implied by the current CLI configuration.
 
-                  #env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+#env=core.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
-                  # Uncomment the next line if you know exactly what Account and Region you
-                  # want to deploy the stack to. */
+# Uncomment the next line if you know exactly what Account and Region you
+# want to deploy the stack to. */
 
-                  #env=core.Environment(account='123456789012', region='us-east-1'),
+#env=core.Environment(account='123456789012', region='us-east-1'),
 
-                  # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                #   )
+# For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+#   )
 # FirstProjectStack(app, "ProdProjectStack", env=env_US, is_prod=True)
 
 app.synth()
